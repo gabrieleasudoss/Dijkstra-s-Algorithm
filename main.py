fb57e3ae69cf4e6cb90ec38153bc068bff67afb3
PSIOG\gabriel.easudoss <gabriel.easudoss@psiog.com>
2023-09-14 12:27:33 +0530

    updated code

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def makeCommits (days : int):
         # commit 
         os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
 
-        return days * makeCommits(days - 10)
+        return days * makeCommits(days - 13)
 
 makeCommits(365)
